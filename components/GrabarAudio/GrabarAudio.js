ebc39c7ba061b58af75a8e0c15a18e9880062c3d
jenner <jennercalito2016@gmail.com>
2024-09-08 20:27:15 -0600

    Se integra el api de speech to text
    Se integra el api de speech to text

--- components/GrabarAudio/GrabarAudio.js
+++ components/GrabarAudio/GrabarAudio.js
@@ -1,20 +1,20 @@
 import React, { useState, useEffect, useRef } from 'react';
 import { View, Text, TouchableOpacity, StyleSheet, Image, Alert } from 'react-native';
 import { Audio } from 'expo-av';
-import { AnimatedCircularProgress } from 'react-native-circular-progress'; // Necesitas instalar esta librería
-import * as FileSystem from 'expo-file-system';
+import { AnimatedCircularProgress } from 'react-native-circular-progress';
 
 export default function FluidezLectoraComponent() {
-  const [recording, setRecording] = useState(null);
+  
+  const baseUrl = process.env.EXPO_PUBLIC_URL;
+
+  const recordingRef = useRef(null);
   const [isRecording, setIsRecording] = useState(false);
   const [recordedURI, setRecordedURI] = useState(null);
   const [timeLeft, setTimeLeft] = useState(60); // 1 minuto
   const [phraseIndex, setPhraseIndex] = useState(0);
-  const recordingRef = useRef(null);
 
-  // Ejemplo de frases para que el usuario lea
-  const textToRead = "Este es un ejemplo de una frase corta. Otra frase para medir la fluidez lectora. Sigue leyendo para completar la prueba. La lectura fluida es importante para comprender. Leer en voz alta mejora la comprensión. Cada día es una oportunidad para aprender. La práctica constante te hará un lector fluido. La lectura es la llave del conocimiento. Concentrarse en las palabras mejora la fluidez. Un buen lector entiende y disfruta el texto. Las palabras tienen el poder de cambiar el mundo. Leer en voz alta te ayuda a mejorar la pronunciación. La fluidez lectora se logra con perseverancia. Las frases cortas son ideales para practicar la lectura. Leer con precisión es tan importante como leer rápido. La velocidad lectora es clave para una buena comprensión. Los buenos lectores disfrutan de las historias escritas. La lectura es una habilidad que se perfecciona con el tiempo. Las palabras bien pronunciadas transmiten mejor el mensaje. Cada palabra cuenta en la fluidez lectora. La lectura es un viaje a mundos desconocidos. Las historias están hechas para ser leídas y contadas. La lectura diaria aumenta tu vocabulario. Practicar la lectura en voz alta mejora tu confianza. Leer es una habilidad esencial para el éxito académico. Los libros son una ventana al conocimiento. La práctica hace al maestro, también en la lectura. Las palabras escritas tienen un poder increíble. Leer bien es un arte que se desarrolla. La lectura rápida y fluida se logra con práctica diaria. Las frases bien construidas hacen la lectura más fácil. Un buen lector puede capturar la esencia del texto. La lectura te abre puertas a nuevas experiencias. Cada libro leído es un paso más hacia la sabiduría.";
-
+  const textToRead = "Este es un ejemplo de una frase corta.";
+  
   // Dividir el texto en frases usando el punto como delimitador
   const phrasesToRead = textToRead.split('. ').map((phrase) => phrase.trim());
 
@@ -32,40 +32,51 @@ export default function FluidezLectoraComponent() {
     })();
   }, []);
 
-  // Controla la aparición de frases cada 2 segundos
   useEffect(() => {
-    let interval = null;
-    if (isRecording && phraseIndex < phrasesToRead.length) {
-      interval = setInterval(() => {
-        setPhraseIndex((prevIndex) => prevIndex + 1);
-      }, 3000); // 2000 ms = 2 segundos
-    } else if (phraseIndex >= phrasesToRead.length) {
-      stopRecording();
-      clearInterval(interval);
-    }
-    return () => clearInterval(interval);
-  }, [isRecording, phraseIndex]);
+    let phraseInterval = null;
+    let timerInterval = null;
 
-  // Función para manejar el tiempo de lectura
-  useEffect(() => {
-    if (isRecording && timeLeft > 0) {
-      const timer = setInterval(() => {
-        setTimeLeft((prevTime) => prevTime - 1);
+    console.log('is recording ', isRecording)
+    if (isRecording) {
+      phraseInterval = setInterval(() => {
+        setPhraseIndex((prevIndex) => {
+          const nextIndex = prevIndex + 1;
+          if (nextIndex >= phrasesToRead.length) {
+            stopRecording(); // Detener la grabación si se han mostrado todas las frases
+            return prevIndex;
+          }
+          return nextIndex;
+        });
+      }, 3000);
+
+      timerInterval = setInterval(() => {
+        setTimeLeft((prevTime) => {
+          const newTime = prevTime - 1;
+          if (newTime <= 0) {
+            stopRecording(); // Detener la grabación si el tiempo se agota
+            return 0;
+          }
+          return newTime;
+        });
       }, 1000);
-      return () => clearInterval(timer);
-    } else if (timeLeft === 0) {
-      stopRecording();
+    } else {
+      clearInterval(phraseInterval);
+      clearInterval(timerInterval);
     }
-  }, [isRecording, timeLeft]);
 
-  // Iniciar grabación
+    return () => {
+      clearInterval(phraseInterval);
+      clearInterval(timerInterval);
+    };
+  }, [isRecording]);
+
   async function startRecording() {
     try {
+      console.log("comienza la grabada")
       const recording = new Audio.Recording();
       await recording.prepareToRecordAsync(Audio.RECORDING_OPTIONS_PRESET_HIGH_QUALITY);
       await recording.startAsync();
       recordingRef.current = recording;
-      setRecording(recording);
       setIsRecording(true);
       setTimeLeft(60); // Reiniciar el tiempo a 1 minuto
       setPhraseIndex(0); // Reiniciar el índice de frases
@@ -74,7 +85,6 @@ export default function FluidezLectoraComponent() {
     }
   }
 
-  // Detener grabación y enviar archivo a la API
   async function stopRecording() {
     if (recordingRef.current) {
       setIsRecording(false);
@@ -84,49 +94,42 @@ export default function FluidezLectoraComponent() {
         setRecordedURI(uri);
         recordingRef.current = null;
         console.log('Audio grabado guardado en:', uri);
-
         // Subir el archivo a la API
         await uploadAudio(uri);
-
-        // Reproducir el archivo grabado (opcional)
-        const { sound } = await Audio.Sound.createAsync(
-          { uri: uri },
-          { shouldPlay: false }
-        );
-        setSound(sound);
-
       } catch (err) {
         console.error('Error al detener la grabación:', err);
       }
     }
-  }
 
-  // Función para subir el archivo de audio a la API
-  async function uploadAudio(uri) {
-    const formData = new FormData();
-    formData.append('file', {
-      uri: uri,
-      type: 'audio/m4a', // Asegúrate de usar el tipo MIME correcto según el formato de tu archivo
-      name: 'recording.m4a', // Nombre del archivo
-    });
+    async function uploadAudio(uri) {
+      const formData = new FormData();
+      
+      // Obtén el archivo desde la URI
+      const file = {
+        uri: uri,
+        name: 'output.mp3', // Cambia la extensión según el formato de grabación
+        type: 'audio/mp3' // Cambia el tipo MIME según el formato de grabación
+      };
+    
+      formData.append('file', file);
+      formData.append('texto', textToRead)
 
-    try {
-      const response = await fetch('https://your-api-endpoint.com/upload', {
-        method: 'POST',
-        body: formData,
-        headers: {
-          'Content-Type': 'multipart/form-data',
-        },
-      });
-
-      if (!response.ok) {
-        throw new Error('Error en la carga');
+      console.log('Formulario ', formData)
+    
+      try {
+        const response = await fetch(`${baseUrl}/speach-to-text/compare-by-audio`, {
+          method: 'POST',
+          body: formData,
+          headers: {
+            'Content-Type': 'multipart/form-data',
+          },
+        });
+    
+        const result = await response.json();
+        console.log('Upload success:', result);
+      } catch (err) {
+        console.error('Error uploading audio:', err);
       }
-
-      const result = await response.json();
-      console.log('Respuesta del servidor:', result);
-    } catch (error) {
-      console.error('Error al subir el archivo:', error);
     }
   }
 
@@ -141,7 +144,7 @@ export default function FluidezLectoraComponent() {
       <AnimatedCircularProgress
         size={200}
         width={15}
-        fill={(60 - timeLeft) * (100 / 60)} // Calcula el progreso en base al tiempo restante
+        fill={(60 - timeLeft) * (100 / 60)}
         tintColor="#2A6F97"
         backgroundColor="#f0f0f0"
         style={styles.circularProgress}
@@ -153,7 +156,7 @@ export default function FluidezLectoraComponent() {
             onPressOut={stopRecording}
           >
             <Image
-              source={require('../../assets/Microfono.png')} // Ruta de la imagen de micrófono
+              source={require('../../assets/Microfono.png')}
               style={styles.micImage}
             />
           </TouchableOpacity>
@@ -181,6 +184,11 @@ const styles = StyleSheet.create({
     fontSize: 30,
     fontWeight: 'bold',
     color: '#2A6F97',
+  },
+  subHeaderText: {
+    fontSize: 18,
+    marginTop: 10,
+    color: '#000',
   },
   phraseToRead: {
     marginTop: 20,
